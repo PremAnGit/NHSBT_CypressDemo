f5ac87814585f4c80fe1499e7352dcd1ee42990b
PremAnGit <anandan.prem@gmail.com>
2025-11-19 23:08:04 +0000

    added an integration test

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,10 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://automationexercise.com',
     homepageUrl:'https://automationexercise.com/',
-    specPattern:'cypress/e2e/**/*.cy.js',
+    specPattern: [
+      'cypress/e2e/**/*.cy.js',               // your existing e2e tests
+      'cypress/integrationTests/**/*.cy.js',  // your “integrationTests” folder
+    ],
     supportFile: 'cypress/support/e2e.js',
     screenshotOnRunFailure: true,
     screenshotsFolder: 'cypress/screenshots',
@@ -16,6 +19,11 @@ module.exports = defineConfig({
       html: false,
       json: true,
       charts: true
+    },
+    env:
+    {
+      apiUrl:'https://automationexercise.com/api',
+      productList:'/productsList'
     },  
     setupNodeEvents(on, config) {
       // implement node event listeners here
